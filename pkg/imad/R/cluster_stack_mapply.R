1053b4397b85628d23925907c08f84c513e55806
jgrn307 (Dr. ) <jgreenberg@unr.edu>
2012-05-02 21:49:05 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/imad@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/imad/R/cluster_stack_mapply.R
+++ pkg/imad/R/cluster_stack_mapply.R
@@ -20,24 +20,24 @@ cluster_stack_mapply <- function(x, fun, args=NULL, filename='', cl=NULL, m=2, .
 	
 	x_list=raster_to_list(x)
 	
-	if (!is.null(args)) {
-		stopifnot(is.list(args))
-		
-		clusfun <- function(fun, i) {
-#			r <- crop(x, extent(out, r1=tr$row[i], r2=tr$row2[i], c1=1, c2=ncol(out)))
-			r <- do.call(fun, c(r, args))
-#			getValues(r)
-		}
-		
-	} else {
-		
-		clusfun <- function(fun, i) {
-#			r <- crop(x, extent(out, r1=tr$row[i], r2=tr$row2[i], c1=1, c2=ncol(out)))
-			r <- fun(r)
-#			getValues(r)
-		}
-	}
+#	if (!is.null(args)) {
+#		stopifnot(is.list(args))
+#		
+#		clusfun <- function(fun, i) {
+##			r <- crop(x, extent(out, r1=tr$row[i], r2=tr$row2[i], c1=1, c2=ncol(out)))
+#			r <- do.call(fun, c(r, args))
+##			getValues(r)
+#		}
+#		
+#	} else {
+#		
+#		clusfun <- function(fun, i) {
+##			r <- crop(x, extent(out, r1=tr$row[i], r2=tr$row2[i], c1=1, c2=ncol(out)))
+#			r <- fun(r)
+##			getValues(r)
+#		}
+#	}
 	
-	x_out = stack(clusterMap(cl,fun=clusfun,x=x_list))
+	x_out = stack(clusterMap(cl,fun=clusfun,x=x_list,MoreArgs=args))
 	return(x_out)
 }
